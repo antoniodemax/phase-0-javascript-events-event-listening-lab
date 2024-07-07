375b9c84ab26e13d03f32d42cd1487d038aadc41
Anthony <anthony.onyango@student.moringaschool.com>
2024-07-07 16:32:58 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    //Get the element
+    const input = document.getElementById('button');
+
+    //An event listener is being defined inorder to be called(anonymous/named function)
+    function clickAlert() {
+        alert('click me!');
+    }
+    input.addEventListener('click', clickAlert);
 }
+    addingEventListener();//call the function
+
